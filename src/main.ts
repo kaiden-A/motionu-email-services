0ffcafb567b3e442d0f7d8eaa7458917c72f4a9c
Kaiden-A <amirikhwanfaisal@gmail.com>
2026-05-29 21:17:16 +0800

    update cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies to be sent back and forth if needed
+  });
+
   app.setGlobalPrefix('api/v1')
   await app.listen(process.env.PORT ?? 8080);
 }
